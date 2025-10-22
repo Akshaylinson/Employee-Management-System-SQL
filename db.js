2d14e0ad25c1fe82b41b383d67491bf45465fca5
AKSHAY LINSON M <158825577+Akshaylinson@users.noreply.github.com>
2025-10-22 21:44:45 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,16 +1,1 @@
-// src/db.js
-const { Pool } = require('pg');
-require('dotenv').config();
 
-const pool = new Pool({
-  host: process.env.PGHOST || 'localhost',
-  port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE
-});
-
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-  pool
-};
